db0fcc8e7f1971986f891388119d141fdbbcb5ab
sonianavrom <sonianavrom1@gmail.com>
2026-05-05 12:10:01 +0200

    Subimos dataframe2 definitivo

--- sql/Dataframe_2_CatalogoPeliculas.sql
+++ sql/Dataframe_2_CatalogoPeliculas.sql
@@ -1,7 +1,7 @@
 USE sakila;
 
 SELECT
-	f.film_id,
+    f.film_id,
     f.title AS Pelicula,
     f.description AS Descripcion,
     f.release_year AS Año_lanzamiento,
@@ -24,9 +24,9 @@ SELECT
 -- Normalizamos títulos y descripciones
     f.film_id,
     CONCAT(UPPER(LEFT(f.title,1)), LOWER(SUBSTRING(f.title,2))) AS title_clean,
-	CONCAT(UPPER(LEFT(f.description,1)), LOWER(SUBSTRING(f.description,2))) AS description_clean,
+    CONCAT(UPPER(LEFT(f.description,1)), LOWER(SUBSTRING(f.description,2))) AS description_clean,
 -- Unificamos nombres de categoría e idioma
-	CONCAT(UPPER(LEFT(c.name,1)), LOWER(SUBSTRING(c.name,2))) AS category_name, 
+    CONCAT(UPPER(LEFT(c.name,1)), LOWER(SUBSTRING(c.name,2))) AS category_name, 
     CONCAT(UPPER(LEFT(l.name,1)), LOWER(SUBSTRING(l.name,2))) AS language_name,
     f.length,
     f.rating,
@@ -36,10 +36,10 @@ FROM film f
 INNER JOIN film_category fc ON f.film_id = fc.film_id
 INNER JOIN category c ON fc.category_id = c.category_id
 INNER JOIN language l ON f.language_id = l.language_id
-INNER JOIN inventory i ON f.film_id = i.film_id
+LEFT JOIN inventory i ON f.film_id = i.film_id
 -- Eliminamos registros con duración 0 y/o valor nulo
 WHERE 
-	f.length > 0 
-	AND f.rating IS NOT NULL;
+    f.length > 0 
+    AND f.rating IS NOT NULL;
 
-SELECT * FROM Vista_catalogo LIMIT 10;
+SELECT * FROM Vista_catalogo;
